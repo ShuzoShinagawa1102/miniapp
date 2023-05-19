43362b06a8ebfec01298ae681c07ffa7c5807b3c
Shuzo <cssz20040@g.nihon-u.ac.jp>
2023-05-19 15:55:50 +0900

    フォルダのアップロード

---
+++ test.js
@@ -0,0 +1,19 @@
+console.log();
+
+//関数
+const func1 = (x,y) => {
+    return x + y;
+}
+
+const list = [1,2,3]
+console.log(list[0]);
+
+const object = {
+    name:"yusaku",
+    age:11
+};
+console.log(object.name);
+object.name = "Shuzo";
+
+
+
