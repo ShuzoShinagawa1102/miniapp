a2a40106b6e45593e017e5e3e2fd6d1f0ebc15e7
Shuzo <cssz20040@g.nihon-u.ac.jp>
2023-06-02 16:18:47 +0900

    s

--- TEST_PROJECT/src/components/page-map/page-map.vue
+++ TEST_PROJECT/src/components/page-map/page-map.vue
@@ -1,15 +1,25 @@
 <template>
-    <div id="map" class="map"></div>
+    <view class="map">
+        <image src="/static/map.png">
+
+        </image>
+    </view>
 </template>
 
 <script async defer
- setup lang="ts"
- src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDDiCLXghb5ALx0FvTuHTw40dO2hn5f3_8&callback=initMap">
-</script>
+    setup lang="ts"
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDDiCLXghb5ALx0FvTuHTw40dO2hn5f3_8&callback=initMap">
 
+    function initMap() {
+    var opts = {
+        zoom: 15,
+        center: new google.maps.LatLng(35.709984,139.810703)
+    };
+        var map = new google.maps.Map(document.getElementById("map"), opts);
+    }
+</script>
 <style scoped>
-  .map {
-    width: 100vw;
-    height: 100vh;
-  }
+
+
 </style>
+  
